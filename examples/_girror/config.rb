7e25fc299966d61c6fa3d64cbaec475805acdd61
Pavel Argentov <argentoff@gmail.com>
2011-09-06 18:31:11 +0400

    Experimenting with Travis CI

--- examples/_girror/config.rb
+++ examples/_girror/config.rb
@@ -7,10 +7,10 @@ module Config
   OPTIONS = {
     # custom commit message
     :commit_msg => Proc.new {"State at #{Time.now}"},
-  
+
     # local filename encoding
     :lenc => 'koi8-r',
-    
+
     # remote filename encoding
     :renc => 'cp1251',
 
@@ -22,7 +22,7 @@ module Config
       # specific ssh secret key files are listed in this array
       :keys        => ['/home/paul/.ssh/id_dsa_somekey'],
 
-      # you may set this to true or one of algo settings 
+      # you may set this to true or one of algo settings
       # (see docs for Net::SSH.start)
       :compression => true
     }
